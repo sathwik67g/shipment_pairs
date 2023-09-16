e6834403de4d26d74d89da968b05acb1bad28646
SathwikGampala <sathwik.gampala67@gmail.com>
2023-09-16 11:11:46 -0500

    Update shipment_pairs.py
    completed shipment

--- src/shipment_pairs.py
+++ src/shipment_pairs.py
@@ -31,10 +31,9 @@ SHIPMENTS = [
 
 
 def remove_intra(shipments):
-    """remove_intra removes any records in which the origin and destination are the
-    same country.
-    """
-    pass
+    filtered_shipments = [shipment for shipment in shipments ]
+    if shipment[0] != shipment[1]
+    return filtered_shipments
 
 
 def collect_pairs(shipments):
